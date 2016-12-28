801c62e2eace4c90ffc0299900a8a0cbb36230ee
Li Gaozhao <ligaozhao@icloud.com>
2016-12-28 15:00:44 +0800

    Include the log4j

--- src/main/java/com/gavin/servlet/login/loginServlet.java
+++ src/main/java/com/gavin/servlet/login/loginServlet.java
@@ -8,18 +8,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class loginServlet extends HttpServlet {
+	
+	private static final Logger log = LoggerFactory.getLogger(loginServlet.class);
 	
 	private static final long serialVersionUID = 696801515720053461L;
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse rep)
 			throws ServletException, IOException {
-		System.out.println("User Name --> " + req.getParameter("userName"));
-		System.out.println("Password --> " + req.getParameter("password"));
-		PrintWriter pw = rep.getWriter();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		log.debug("Entered the doGet Method");
+		log.debug("User Name --> " + req.getParameter("userName"));
+		log.debug("Password --> " + req.getParameter("password"));
+
+		PrintWriter pw = resp.getWriter();
+		pw.println("Processed by the servlet alreadt!");
+		pw.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		log.debug("Entered the doPost Method");
+		log.debug("User Name --> " + req.getParameter("userName"));
+		log.debug("Password --> " + req.getParameter("password"));
+		
+		PrintWriter pw = resp.getWriter();
 		pw.println("Here it is!");
 		pw.close();
 	}
+	
+	
 
 }
